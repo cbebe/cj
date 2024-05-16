ab0067b16d7b8d2d9f5d0b4f4a1237265fed3580
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2024-05-16 08:53:17 -0600

    Finish basic lookup

---
+++ main.js
@@ -0,0 +1,35 @@
+let allChars = {};
+async function loadJSON() {
+  const data = await fetch('/data/groups.json').then((r) => r.json());
+  for (const i in data) {
+    for (const j in data[i]) {
+      allChars[j] = data[i][j];
+    }
+  }
+}
+
+loadJSON();
+
+/**
+ * @type {HTMLInputElement}
+ */
+const input = document.querySelector('input');
+
+const ul = document.querySelector('ul');
+
+input.addEventListener('submit', (e) => e.preventDefault());
+input.addEventListener('input', (e) => {
+  ul.textContent = '';
+  for (const c in e.target.value) {
+    const char = e.target.value[c];
+    if (char in allChars) {
+      const li = document.createElement('li');
+      li.textContent = `${char} - ${allChars[char]} - `;
+      const a = document.createElement('a');
+      a.textContent = '魚';
+      a.href = `plecoapi://x-callback-url/s?q=${encodeURIComponent(char)}`;
+      li.appendChild(a);
+      ul.appendChild(li);
+    }
+  }
+});
---
+++ revprox.go
@@ -0,0 +1,33 @@
+// Based on Simple reverse proxy in Go:
+// https://gist.github.com/JalfResi/6287706
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+	remote, err := url.Parse("http://localhost:5500")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handler := func(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+		return func(w http.ResponseWriter, r *http.Request) {
+			log.Println(r.URL)
+			r.Host = remote.Host
+			w.Header().Set("X-Ben", "Rad")
+			p.ServeHTTP(w, r)
+		}
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	http.HandleFunc("/", handler(proxy))
+	err = http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
